73f8690e170bf1bd0fb2aea62f3b4470794db7c3
ColdWindScholar <3590361911@qq.com>
2024-04-04 19:25:12 +0800

    INIT

--- dna.py
+++ dna.py
@@ -7,10 +7,16 @@ def exception_handler(exception_type, exception, traceback):
     del traceback
     print("很抱歉，工具出现错误， 请把以下日志提交给开发者：")
     sys.stderr.write('{}: {}\n'.format(exception_type.__name__, exception))
-    input("任意按钮退出")
+    a = input("是否重启 [1=重启/0=退出]")
+    if a == "1":
+        init()
+
+
+def init():
+    cyrus.check_permissions()
 
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     sys.excepthook = exception_handler
-    cyrus.check_permissions()
+    init()
