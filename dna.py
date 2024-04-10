4ca5ca60cc24f374ed4cda53d91a6a96a1feda82
ColdWindScholar <3590361911@qq.com>
2024-04-10 11:53:33 +0800

    Update Some Code

--- dna.py
+++ dna.py
@@ -7,9 +7,10 @@ def exception_handler(exception_type, exception, traceback):
     del traceback
     print("很抱歉，工具出现错误， 请把以下日志提交给开发者：")
     sys.stderr.write('{}: {}\n'.format(exception_type.__name__, exception))
-    a = input("是否重启 [1=重启/0=退出]")
-    if a == "1":
+    if input("是否重启 [1=重启/0=退出]") == "1":
         init()
+    else:
+        sys.exit(1)
 
 
 def init():
